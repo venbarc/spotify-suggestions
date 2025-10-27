e874123280f21ddfa35fa7ddc0a88dfc74f5c564
venbarc <118934661+venbarc@users.noreply.github.com>
2025-10-27 17:10:44 +0800

    modify sub titile

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
       <h2 className="text-5xl font-black mb-6 bg-gradient-to-r from-gray-900 to-green-600 bg-clip-text text-transparent">
         Your next loop starts here.
       </h2>
-      <p className="text-xl text-gray-300 max-w-2xl mx-auto font-medium">
+      <p className="text-2xl font-black font-bold text-gray-300 max-w-2xl mx-auto font-medium">
         Enter your 5 favorite artists and discover 10 hidden gem songs
       </p>
     </section>
